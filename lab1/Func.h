aa745fd1c08323dace4862401a7370269585f35e
polyanastasia <73426393+fracktus@users.noreply.github.com>
2024-03-18 02:08:30 +0300

    Create Func.h

---
+++ lab1/Func.h
@@ -0,0 +1,8 @@
+#pragma once
+#pragma warning(disable: 4996)
+#include <stdlib.h> 
+#include <stdio.h>
+
+int* Read(int a); 
+void Finder(int n, int A, const int** M, const int* b, int* cost);
+int** Matrix( const int* b, const int* c, const int A, const int n);
