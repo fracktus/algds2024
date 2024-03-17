02f9ca3351b5699a31f504d9dd0f8f09c8a0bfc8
polyanastasia <73426393+fracktus@users.noreply.github.com>
2024-03-18 02:10:43 +0300

    Update Func.h

--- lab1/Func.h
+++ lab1/Func.h
@@ -4,5 +4,5 @@
 #include <stdio.h>
 
 int* Read(int a); 
-void Finder(int n, int A, const int** M, const int* b, int* cost);
+void Finder(int n, int A, const int** M, const int* b, int* F);
 int** Matrix( const int* b, const int* c, const int A, const int n);
